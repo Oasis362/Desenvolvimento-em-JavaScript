785ae089a1a168c9928849dbc18327d47de12352
Oasis362 <oasis363.201007@gmail.com>
2026-02-18 21:55:41 -0300

    Add files via upload

---
+++ Aula 10/Atividades/script.js
@@ -0,0 +1,89 @@
+//  1 - Positivo, Negativo e Neutro
+
+// let numero = Number(prompt('Digite um número'));
+
+// if (numero == 0 ){
+//     console.log('O número é Neutro')
+// } else if(numero < 0){
+//     console.log('O número é Negativo')
+// } else {
+
+//     console.log('O número é positivo')
+//}
+
+//  2 - Poder de Voto
+
+// let idade = Number(prompt('Digite sua idade'))
+
+// if (idade >= 16){
+//     console.log('Pode votar!')
+// }else{
+//     console.log('Não pode votar')
+// }
+
+// 3 - Impar ou Par
+
+// let numero1 = Number(prompt('Digite um número'))
+
+// if (numero1 % 2 == 0){
+//     console.log('É um número par!')
+// }
+// else{
+//     console.log('É um número impar!')
+// }
+
+// 4 - Triângulos
+
+// let num1 = Number(prompt('Digite um Número'))
+// let num2 = Number(prompt('Digite um Número'))
+// let num3 = Number(prompt('Digite um Número'))
+
+// if (num1 == num2 && num1 == num3 && num2 && num3){
+//     console.log('É um triângulo Equilátero(todos lados iguais)')
+// }
+// else if (num1 == num2 || num1 == num3 && num2 != num3 || num2 != num1){
+//     console.log('É um triângulo isósceles(dois lados iguais)')
+// }
+// else{
+//     console.log('É um triângulo Escaleno')
+// }  
+
+//  5 - Multiplo de 8 e 16
+
+// let n1 = Number(prompt('Digite um número'))
+
+// if (n1 % 8 == 0 && n1 % 16 == 0){
+//     console.log('É um multiplo')
+// }
+// else{
+//     console.log('Não é um multiplo')
+// }
+
+//  6 - Número Positivo maior que 15
+
+// let nu1 = Number(prompt('Digite um número'))
+
+// if (nu1 > 15){
+//     console.log('Atende as condições')
+// }
+// else{
+//     console.log('Não atende as condições')
+// }
+
+//  7 - Divisível por 9 ou 6
+
+let nume1 = Number(prompt('Digite um número'))
+
+if (nume1 % 9 == 0 || nume1 % 6 == 0){
+    console.log('É um divisor')
+}
+else{
+    console.log('Não é um divisor')
+}
+
+
+
+
+
+
+
